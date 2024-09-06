330f15734c52b0bd55e1f0831099035572c6a64f
Riley Adair <rileyadair@Rileys-MacBook-Pro-2.local>
2024-09-06 11:48:35 -0500

    Remove test code

--- src/apollo-server.ts
+++ src/apollo-server.ts
@@ -1,48 +1,10 @@
 import { ApolloServer, gql } from "apollo-server-lambda";
-// const {
-//   ApolloServerPluginLandingPageGraphQLPlayground,
-// } = require("apollo-server-core");
-// import { todo } from "./resolvers/queries/todo";
-// import { todos } from "./resolvers/queries/todos";
 import { typeDefs } from "./types";
 import { resolvers } from "./resolvers";
-
-// Construct a schema, using GraphQL schema language
-// const typeDefs = gql`
-//   type Todo {
-//     id: String
-//     text: String
-//     checked: Boolean
-//     createdAt: Int
-//     updatedAt: Int
-//   }
-//   type Query {
-//     hello: String
-//     todo(id: ID!): Todo!
-//     todos: [Todo]
-//   }
-// `;
-
-// Provide resolver functions for your schema fields
-// const resolvers = {
-//   Query: {
-//     hello: () => "Hello world!",
-//     todo: todo,
-//     todos: todos,
-//   },
-// };
 
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-
-  // By default, the GraphQL Playground interface and GraphQL introspection
-  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
-  //
-  // If you'd like to have GraphQL Playground and introspection enabled in production,
-  // install the Playground plugin and set the `introspection` option explicitly to `true`.
-  // introspection: true,
-  // plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
 export default apolloServer.createHandler();
